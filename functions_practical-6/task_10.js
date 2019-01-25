3a131a73ce441c51006c325328d88c30da2862b3
BIT Student <classroom@bgit.rs>
2019-01-25 16:01:06 +0100

    solved_task_10

--- functions_practical-6/task_10.js
+++ functions_practical-6/task_10.js
@@ -5,14 +5,44 @@ function mostfrequentElement(someArray) {
   let mostfrequent;
   let index;
   let counter = 0;
-  let temp;
+  let temp = 0;
   for (let i = 0; i < someArray.length; i++) {
     mostfrequent = someArray[i];
     for (let j = 0; j < someArray.length; j++) {
       if (mostfrequent === someArray[j]) {
         counter++;
-        index = i;
       }
     }
+    if (counter > temp) {
+      temp = counter;
+      index = someArray[i];
+    }
+    counter = 0;
   }
+  console.log(temp);
+  console.log(index);
 }
+console.log(
+  mostfrequentElement([
+    4,
+    1,
+    5,
+    5,
+    5,
+    5,
+    5,
+    2,
+    3,
+    4,
+    5,
+    6,
+    5,
+    6,
+    8,
+    8,
+    4,
+    1,
+    4,
+    4
+  ])
+);
